3a9a8e1d5061d3b33afa06635a3bab6d14131b68
Hany <parkhanhee89@gmail.com>
2017-09-23 01:26:50 -0400

    Add Member class

---
+++ NF.Shared/Member.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace NF.Shared
+{
+    /// <summary>
+    /// Member.
+    /// </summary>
+    public class Member
+    {
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:NF.Shared.Member"/> class.
+        /// </summary>
+        public Member()
+        {
+        }
+        #endregion
+    }
+}
