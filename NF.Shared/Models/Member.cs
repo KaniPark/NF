f4d2372a10c140dbcb90da88b31abd318c2df4f1
Hany <parkhanhee89@gmail.com>
2017-09-23 23:18:51 -0400

    Add Member class

---
+++ NF.Shared/Models/Member.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NF.Models
+{
+    public class Member
+    {
+        public Member()
+        {
+        }
+
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public string PhoneNumber
+        {
+            get;
+            set;
+        }
+
+        public DateTime DateOfBirth
+        {
+            get;
+            set;
+        }
+
+		public int Age
+		{
+			get;
+			set;
+		}
+    }
+}
